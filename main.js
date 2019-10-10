6907b00115da5dfa69bc389f0664001e8d6bc03b
dvisotsky <37811325+dvisotsky@users.noreply.github.com>
2019-10-09 23:41:17 -0400

    choose file to upload

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-var TEXT_FILE_PATH = 'whuteva.txt';
+window.onload=function(){
+  document.getElementById('input-file').addEventListener('change', getFile)
+}
 
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                document.getElementById("textFile").innerHTML = allText;
-                //alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
+function getFile(event) {
+	const input = event.target
+  if ('files' in input && input.files.length > 0) {
+	  placeFileContent(
+      document.getElementById('content-target'),
+      input.files[0])
+  }
+}
+
+function placeFileContent(target, file) {
+	readFileContent(file).then(content => {
+  	target.value = content
+  }).catch(error => console.log(error))
+}
+
+function readFileContent(file) {
+	const reader = new FileReader()
+  return new Promise((resolve, reject) => {
+    reader.onload = event => resolve(event.target.result)
+    reader.onerror = error => reject(error)
+    reader.readAsText(file)
+  })
 }
